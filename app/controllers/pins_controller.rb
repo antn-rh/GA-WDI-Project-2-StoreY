241cee7e372f387cff598babf097592e9dd2476a
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-31 00:25:25 -0700

    updates pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,48 @@
 class PinsController < ApplicationController
+  before_action :set_pin, only: [:edit, :show, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to user_story_pins_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @pin.update_attributes(pin_params)
+      redirect_to @pin
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to user_story_pins_path
+  end
+
+private
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def pin_params
+    params.require(:pin).permit(:user_id, :story_id, :text)
+  end
 end
