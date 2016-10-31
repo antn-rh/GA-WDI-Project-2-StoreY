ea500ffcbf5d07d6f7891317e767d19a58d5adbc
Antonio Rhee <antoniolrhee@gmail.com>
2016-10-31 12:47:24 -0700

    changes to pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,23 +2,27 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:edit, :show, :update, :destroy]
 
   def index
-    @pins = Pin.all
+    @pins = Story.find(params[:story_id]).pins
+    @story = Story.find(params[:story_id])
   end
 
   def new
+    @story = Story.find(params[:story_id])
     @pin = Pin.new
-  end
-
-  def edit
   end
 
   def show
   end
 
+  def edit
+  end
+
   def create
+    @story = Story.find(params[:story_id])
     @pin = Pin.new(pin_params)
+    @pin.story = @story
     if @pin.save
-      redirect_to user_story_pins_path
+      redirect_to user_story_pins_path(@user, @story)
     else
       render :new
     end
@@ -40,6 +44,7 @@ class PinsController < ApplicationController
 private
   def set_pin
     @pin = Pin.find(params[:id])
+    @story = Story.find(params[:story_id])
   end
 
   def pin_params
--- config/routes.rb
+++ config/routes.rb
@@ -11,25 +11,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # get 'users/' => 'users#index'
-  # post 'users/' => 'users#create'
-  # get 'users/new' => 'users#new', as: :new_user
-  # get 'users/:id' => 'users#show', as: :user
-  # get 'users/:id/edit' => 'users#edit', as: :edit_user
-  # patch 'users/:id' => 'users#update'
-  # delete 'users/:id' => 'users#destroy'
-  #
-  # get 'users/:user_id/stories' => 'stories#index', as: :user_stories
-  # post 'users/:user_id/stories' => 'stories#create'
-  # get 'users/:user_id/stories/new' => 'stories#new', as: :new_user_story
-  # get 'users/:user_id/stories/:id' => 'stories#show', as: :user_story
-  # get 'users/:user_id/stories/:id/edit' => 'stories#edit', as: :edit_user_story
-  # patch 'users/:user_id/stories/:id' => 'stories#update'
-  # delete 'users/:user_id/stories/:id' => 'stories#destroy'
-
-
-  # how to create routes for comments?
-
   get '/login'     => 'sessions#new'
   post '/login'    => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
