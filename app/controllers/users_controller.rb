41ee744ef7a8f8690d9123e1752e0b79e9a7da63
Antonio Rhee <antoniolrhee@gmail.com>
2017-01-23 22:22:19 -0800

    commits from project week

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # have a second before_action for index, new, etc.
 
   def welcome
     @user = User.new
